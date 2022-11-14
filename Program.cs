26cb4e48d2d012b230293d5d39641d7b025be6f9
AmrDhaliwal <amritdhaliwalz@gmail.com>
2022-11-14 12:01:18 -0800

    Initial project commit.
    Initial commit to the start of the Pokedex CLI project.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace PokedexCLI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("PokedexCLI project boilerplate.");
+        }
+    }
+}
